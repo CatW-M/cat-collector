9e7d407b6ae7ed6fdac72bcda87aef4042df8e74
CatW-M <wheelermushlin@gmail.com>
2022-11-29 23:00:18 -0700

    feat: added blog and contact

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
+    path('cats/', views.cats, name='My cats'),
     path('contact/', views.contact, name='contact me'),
     path('blog/', views.blog, name='My blog'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -2,16 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('<h1>Hello World!/ᐠ｡‸｡ᐟ\ﾉ</h1>')
+    return render(request, 'index.html')
 
 def about(request):
-    return HttpResponse('<h1>I am Catherine the developer</h1>')
+    return render(request, 'about.html')
 
 def contact(request):
-    return HttpResponse('<h1>Hey, I just met you, this is crazy, here''s my number, call me maybe? 555-555-555</h1>')
+    return render(request, 'contact.html')
 
 def blog(request):
-    return HttpResponse('<h1>This is my blog</h1>')
+    return render(request, 'blog.html')
+
+def cats(request):
+    return render(request, 'cats.html')
 
 # Create your views here.
 #similar to controllers in javascript...this is like an index file
