3304603d8acaa20b92bdb384b11d50f67748e96b
CatW-M <wheelermushlin@gmail.com>
2022-11-29 23:42:05 -0700

    feat: created Cat Class

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+class Cat:
+    def __init__(self, name, breed, description, age):
+        self.name = name
+        self.breed = breed
+        self.description = description
+        self.age = age
+    def __str__(self):
+        return f"{self.name}"
+
+cats = [
+    Cat('Lolo', 'tabby', 'foul little demon', 3),
+    Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
+    Cat('Raven', 'black tripod', '3 legged cat', 4),
+]
+
 def index(request):
     return render(request, 'index.html')
 
@@ -13,8 +28,9 @@ def contact(request):
 def blog(request):
     return render(request, 'blog.html')
 
-def cats(request):
-    return render(request, 'cats.html')
+def cats_index(request):
+    return render(request, 'cats/index.html', {'cats': cats})
+
 
 # Create your views here.
 #similar to controllers in javascript...this is like an index file
